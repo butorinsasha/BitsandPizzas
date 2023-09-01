2f053f595ff641d5796fb052b4512a3e02d90e9f
a.b.butorin <a.b.butorin@tinkoff.ru>
2023-09-01 21:02:38 +0300

    Holo Theme applied in my phone right after I deleted  values-night folder

--- app/src/main/java/local/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/local/hfad/bitsandpizzas/MainActivity.java
@@ -1,13 +1,31 @@
 package local.hfad.bitsandpizzas;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import static android.widget.Toast.LENGTH_SHORT;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        Toast.makeText(this, "Action Bar item clicked", LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(menuItem);
     }
 }
