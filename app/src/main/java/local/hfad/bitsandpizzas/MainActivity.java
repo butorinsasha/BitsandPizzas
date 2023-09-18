3e6e10ec61c4956a58ea8e1a168c94a409e4f328
a.b.butorin <a.b.butorin@tinkoff.ru>
2023-09-18 18:49:12 +0300

    Huge magic for ActionBar and Drawer done just by typing... Re-explore it in your mind again later...

--- app/src/main/java/local/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/local/hfad/bitsandpizzas/MainActivity.java
@@ -3,21 +3,161 @@ package local.hfad.bitsandpizzas;
 import static android.widget.Toast.LENGTH_SHORT;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.ShareActionProvider;
 
 public class MainActivity extends Activity {
 
     private ShareActionProvider shareActionProvider;
+    private String[] titles;
+    private ListView drawerList;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
+
+    //Listener for drawer list items
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+
+    // MainActivity overridden methods
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView) findViewById(R.id.drawer);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        drawerList.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_activated_1,
+                titles
+        ));
+
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+
+        //Creating ActionBarDrawerToggle
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer
+        ) {
+
+            //Called when a drawer has settled in a completely open state.
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            //Called when a drawer has settles in a completely closed state
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //Enable the Up (Back) button so you can use it for the drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+    }
+
+    // Sync the toggle state after onRestoreInstanceState has occurred.
+    // You need to add this method to yout activity so that the state of the ActionBarDrawerToggle
+    // is in sync with the state of the drawer
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+
+    // You need to add this method to your activity
+    // so that any configuration changes get passed to the ActionBarDrawerToggle
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
+
+                                       // Additional private MainActivity methods
+    private void selectItem(int position) {
+        Fragment fragment;
+        switch(position) {
+            case 1:
+                fragment = new PizzaFragment();
+                break;
+            case 2:
+                fragment = new PastaFragment();
+                break;
+            case 3:
+                fragment = new StoresFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+        }
+        FragmentTransaction fragmentTransaction = getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .addToBackStack(null)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                fragmentTransaction.commit();
+
+        setActionBarTitle(position);
+
+        drawerLayout.closeDrawer(drawerList);
+
+    }
+
+    private void setActionBarTitle(int position) {
+        String title;
+        if (position == 0) {
+            title = getResources().getString(R.string.app_name);
+        } else {
+            title = titles[position];
+        }
+        getActionBar().setTitle(title);
+    }
+
+
+
+
+    // Action bar overridden methods
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the drawer is open, hide action items related to the content view
+        boolean isDrawerOpened = drawerLayout.isDrawerOpen(drawerList);
+        menu.findItem(R.id.action_share).setVisible(!isDrawerOpened);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
@@ -46,6 +186,14 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(menuItem)) {
+            // actionBarDrawerToggle.onOptionsItemSelected(menuItem)
+            // returns true if the ActionBarDrawerToggle has handled being clicked.
+            // If it returns false, this means that another action item in the action bar has been clicked,
+            // and the rest of the code in the activity’s onOptionsItemSelected() method will run.
+            return true;
+        }
+
         switch(menuItem.getItemId()) {
             case R.id.action_create_order:
                 Toast.makeText(this, menuItem.getTitle() + " selected", LENGTH_SHORT).show();
@@ -59,11 +207,8 @@ public class MainActivity extends Activity {
                 Toast.makeText(this, menuItem.getTitle() + " selected", LENGTH_SHORT).show();
                 return true;
             default:
-                super.onOptionsItemSelected(menuItem);
-
+                Toast.makeText(this, "Action Bar default option item", LENGTH_SHORT).show();
+                return super.onOptionsItemSelected(menuItem);
         }
-
-        Toast.makeText(this, "Action Bar item clicked", LENGTH_SHORT).show();
-        return super.onOptionsItemSelected(menuItem);
     }
 }
